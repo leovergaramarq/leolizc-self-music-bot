66f13b9961066f59a0aebfd8a441b5658ec359a8
Leonardo Lizcano <74639893+LeoLizc@users.noreply.github.com>
2024-08-24 05:21:46 +0000

    feat(packages/discord-bot): add page option to queue command

--- packages/discord-bot/src/player/ux/queue.ts
+++ packages/discord-bot/src/player/ux/queue.ts
@@ -4,12 +4,28 @@ import { EmbedBuilder } from 'discord.js';
 type SongArray = typeof PlaylistManager.prototype.songs;
 
 export interface EmbedQueueOptions {
+  page?: number;
   queue: SongArray;
 }
 
 export default {
-  build: ({ queue }: EmbedQueueOptions) => {
+  build: ({ queue, page = 0 }: EmbedQueueOptions) => {
     let totalDuration = 0;
+
+    if (!queue.length) {
+      return new EmbedBuilder()
+        .setColor(0xfa_a6_1a)
+        .setTitle('🎶Lista de reproducción🎶')
+        .setDescription('No hay canciones en la lista');
+    }
+
+    const pages = Math.ceil(queue.length / 10);
+
+    if (page < 0) {
+      page = 0;
+    } else if (page >= pages) {
+      page = pages - 1;
+    }
 
     for (const song of queue) {
       if (song.duration) {
@@ -22,6 +38,7 @@ export default {
       .slice(11, 19);
 
     const songsString = queue
+      .slice(page * 10, page * 10)
       .map(
         (song, index) =>
           `\`${index + 1}.\` **[${song.title}](${song.url})**\n(${song.duration || 'Desconocido'})`,
@@ -33,6 +50,9 @@ export default {
     return new EmbedBuilder()
       .setColor(0xfa_a6_1a)
       .setTitle('🎶Lista de reproducción🎶')
-      .setDescription(description);
+      .setDescription(description)
+      .setFooter({
+        text: `Página ${page + 1} de ${Math.ceil(queue.length / 10)}`,
+      });
   },
 };
